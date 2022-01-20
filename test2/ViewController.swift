2e2dd970ea1941ff3d187f3ece35d6717290914f
Nguyen Tuan Anh <nguyentuananh@tuananhis-MacBook-Pro.local>
2022-01-20 21:03:20 +0700

    last version ex. back button

--- test2/ViewController.swift
+++ test2/ViewController.swift
@@ -9,21 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var imageView: UIImage!
+    @IBOutlet weak var imageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        title = "View 1"
         // Load Image
         let str = "https://image.winudf.com/v2/image1/Y29tLndhbGxpZS5rYXdhaWlhbmltZWJveS53YWxscGFwZXJzX3NjcmVlbl8xXzE1NzY5MDkzMDhfMDQx/screen-1.jpg?fakeurl=1&type=.jpg"
         let strUrl = URL(string: str)
         if let unstrUrl = strUrl {
             loadImageData(url: unstrUrl)
         }
-        
         // Tapgesture
+        addGesture()
     }
     
+    // MARK: - Load Image from Internet
     func loadImageData(url: URL) {
         let request = URLRequest(url: url)
         URLSession.shared.dataTask(with: request) { data, response, error in
@@ -43,21 +45,24 @@ class ViewController: UIViewController {
             
             DispatchQueue.main.async {
                 if let image2v = UIImage(data: data) {
-                    self.imageView = image2v
+                    self.imageView.image = image2v
                 }
             }
         }.resume()
     }
     
-    
+    // MARK: - add Gesture
     func addGesture() {
         let tapped = UITapGestureRecognizer()
-        tapped.numberOfTapsRequired = 2
+        tapped.numberOfTapsRequired = 1
         tapped.addTarget(self, action: #selector(toView2(_:)))
-        self.imageView.
+        self.imageView.addGestureRecognizer(tapped)
     }
     
     @objc func toView2(_ gesture: UITapGestureRecognizer) {
-        
+        print("tapped to imageView")
+        let vc = ViewController2()
+        vc.lbtext = "OK Im fine"
+        navigationController?.pushViewController(vc, animated: true)
     }
 }
--- test2/ViewController2.swift
+++ test2/ViewController2.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController2: UIViewController {
     @IBOutlet weak var tlb: UILabel!
+    var lbtext: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        title = "view2"
+        
         view.backgroundColor = .green
         tlb.textAlignment = .center
         tlb.textColor = .red
-// Do any additional setup after loading the view.
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "bye", style: .plain, target: nil, action: nil)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .bookmarks, target: nil, action: nil)
+        
+        guard let lbtext = lbtext else {
+            return
+        }
+        tlb.text = lbtext
+
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
     }
 }
